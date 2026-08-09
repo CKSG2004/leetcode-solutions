81303e011a069949c26349746ca0be2a3fc90fbe
G Chinnakarupusamy <chinnakarupusamyg2004@gmail.com>
2026-08-09 23:33:54 +0530

    Auto-Sync: Solved Remove Element (Updated Solution)

--- 0027 - Remove Element/Solution.java
+++ 0027 - Remove Element/Solution.java
@@ -2,13 +2,11 @@ class Solution {
     public int removeElement(int[] a, int val) {
         int c=0;
         for(int i=0;i<a.length;i++){
-            if(a[i]==val){
-                a[i]=51;
-            }else{
+            if(a[i]!=val){
+                a[c] = a[i];
                 c++;
             }
         }
-        Arrays.sort(a);
         return c;
     }
 }
