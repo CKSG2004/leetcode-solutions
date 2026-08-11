380a59c6d62a479969f7976144227ac48b2b2d88
G Chinnakarupusamy <chinnakarupusamyg2004@gmail.com>
2026-08-11 06:21:43 +0530

    Auto-Sync: Solved Maximum Subarray (Updated Solution)

---
+++ 0053 - Maximum Subarray/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int s=0;
+        int ms=nums[0];
+        for(int i=0;i<nums.length;i++){
+            s+=nums[i];
+            if(s>ms){
+                ms=s;
+            }
+            if(s<0){
+                s=0;
+            }
+        }
+        return ms;
+    }
+}
