e091957e9acc8472d3cf751245e397ea944232a4
G Chinnakarupusamy <chinnakarupusamyg2004@gmail.com>
2026-08-19 20:40:44 +0530

    Auto-Sync: Solved Climbing Stairs (Updated Solution)

---
+++ 0070 - Climbing Stairs/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){return n;}
+        int a[]=new int[n+1];
+        a[1]=1;
+        a[2]=2;
+        for(int i=3; i<=n; i++){
+            a[i]=a[i-2]+a[i-1];
+        }
+        return a[n];
+    }
+}
